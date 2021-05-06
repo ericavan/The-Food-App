4e81410379201b23ece82c3df37da83fd4f79f93
ericavan <elv9@georgetown.edu>
2021-05-06 16:52:57 -0400

    created new route

--- routes/users.js
+++ routes/users.js
@@ -15,4 +15,14 @@ router.get('/', function(req, res, next) {
 
 });
 
+router.get('/:id', function(req, res, next) {
+  var userId = req.params.id;
+  console.log("USER ID:", userId)
+
+  var user = {"id": userId, "name": "Example User", "email": "example@example.com"} // just some dummy data
+  res.send(user);
+});
+
+
+
 module.exports = router;
