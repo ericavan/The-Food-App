57501a1fea0e4fa2ef2ad60e47120511ffbe4bdf
ericavan <elv9@georgetown.edu>
2021-05-07 14:08:21 -0400

    updated Yelp API to work with Axios

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fetch = require('node-fetch');
-//const axios = require('axios').default; // first need to run: "npm install axios --save" from the command-line
+const axios = require('axios').default; // first need to run: "npm install axios --save" from the command-line
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -26,26 +26,38 @@ router.get('/hello', function(req, res, next) {
 /* GET Yelp Test page. */
 router.get('/yelp_test', function(req, res, next) {
   var API_KEY = process.env.YELP_API_KEY || "4tOGmvhaVhwWA8BkRHAexxQza0EGS_5ZmIHEjcES2JqPnNO5goFUUHwXS1baEz_nzcvTya-jQJce4hMMpxWAa8PKA3PfWjxlso2tHUGRr-Lr-cBUo3VXdw7yJ1qVYHYx" // TODO: use env vars instead of hard-coding
-  // todo: dynamically compile this url based on form data
   // https://www.yelp.com/developers/documentation/v3/business_search
-  var requestUrl = "https://api.yelp.com/v3/businesses/search?location=20015&price=3"
+  var requestUrl = "https://api.yelp.com/v3/businesses/search"
+  console.log("REQUEST URL:", requestUrl)
+  // https://github.com/axios/axios
   var requestOptions = {
       headers: {
           Authorization: `Bearer ${API_KEY}`,
-          //"Content-type": "application/json",
-      } // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#supplying_request_options
+      },
+      params: {
+        location: "Washington, DC", // todo use form data
+        price: 3 // todo use form data
+      }
   }
-  console.log(requestOptions)
-  fetch(requestUrl, requestOptions)
-      .then(response => response.json())
-      .then(data => {
-          console.log("DATA", data) // this is the data you're looking for
-          // todo: pass the data to the page!
-      })
-      .catch(err => {
-          console.error("FETCH ERR", err)
-      })
-  res.render('hello', {message:"HELLO"}) // todo: use a different page!
+  console.log("REQUEST OPTIONS:", requestOptions)
+  axios.get(requestUrl, requestOptions)
+    .then(function (response) {
+      //console.log("RESPONSE:", response)
+      console.log("DATA:", response.data)
+      //console.log(response.data.businesses)
+      //console.log(response.data.total)
+      //console.log(response.data.region)
+      // todo: flash and render page and pass data to page
+      res.render('hello', {message:"HELLO"}) // todo: use a different page!
+    })
+    .catch(function (error) {
+      console.log("ERR:", error)
+      // todo: flash error message and redirect
+      res.render('hello', {message:"HELLO"}) // todo: use a different page!
+    })
+    .then(function () {
+      console.log("DONE...")
+    })
 });
 
 
